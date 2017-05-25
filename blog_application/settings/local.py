96eebe7ab231e6aaffec9a46b95309d695d3070e
yashLadha <yashladhapankajladha123@gmail.com>
2017-05-25 14:29:16 +0530

    Added model and postgresql backend

--- blog_application/settings/local.py
+++ blog_application/settings/local.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'blog_application.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blog_application',
+        'USER': 'blogadmin',
+        'PASSWORD': 'blogAdmin',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
