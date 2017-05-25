e6c7de49010bd00ad2f1913e7d5fc8186b6ea9d6
yashLadha <yashladhapankajladha123@gmail.com>
2017-05-25 14:29:16 +0530

    Added model and postgresql backend

--- blog/models.py
+++ blog/models.py
@@ -2,4 +2,18 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    """ Model representation of post
+    initDate    : initialization Date
+    tags        : tag of the post
+    title       : title of the post
+    description : main content of the post
+    """
+    initDate = models.DateTimeField()
+    tags = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)
+    descriptiom = models.TextField()
+
+    def __str__(self):
+        return str(self.id) + " " + self.title
