c11088729c3aa9a8cbb8be2a5daea401c7e46542
yashLadha <yashladhapankajladha123@gmail.com>
2017-06-12 19:15:07 +0530

    Updated Settings for Static files

--- blog_application/settings/production.py
+++ blog_application/settings/production.py
@@ -126,7 +126,7 @@ PROJECT_ROOT = os.path.dirname(BASE_DIR)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
